22a40223ce37851085e673bdeef766bbf78167ba
tonioloo <gabriel.toniolo1@outlook.com>
2024-08-02 13:09:01 -0300

    Estudos de conversão de tipo

---
+++ src/br/com/gtoniolo/Classe.java
@@ -0,0 +1,15 @@
+package br.com.gtoniolo;
+
+public class Classe {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println(a);
+
+        Integer b = Integer.valueOf(a);
+
+        System.out.println(b);
+    }
+}
